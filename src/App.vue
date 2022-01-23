49294f1a7a5a449d0c73de0bb22c3f107be9c6f5
reznikovas <ramdirt@mail.ru>
2022-01-23 21:55:23 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+const user = ''
+const stack = computed(() => {
+    return store.state.stack
+})
+</script>
+
+<template lang="pug">
+router-view.p-24
+</template>
+
+<style lang="scss">
+
+</style>
